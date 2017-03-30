aa7654cc503a2b80698d8724c0d0252c0c15c7e1
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-03-31 00:35:18 +0530

    adds moment

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ program
 	.parse(process.argv); // end with parse to parse through the input
 
 if (program.wake) {
-	var timeLeft = new Date(program.wake) - new Date();
+	var now = new Date();
+	var then = new Date(program.wake);
+	var timeLeft = then - now;
+	lme.s(then, now, timeLeft);
+
 	setTimeout(function() {
 		doIt();
 	}, timeLeft);
